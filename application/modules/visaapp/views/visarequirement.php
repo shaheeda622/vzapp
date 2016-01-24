0233d2c10597889c0b1cf9e9c7667f9889fd2839
shaheeda622 <shaheeda622@gmail.com>
2016-01-24 11:56:37 +0400

    visa requirement html done

--- application/modules/visaapp/views/visarequirement.php
+++ application/modules/visaapp/views/visarequirement.php
@@ -81,11 +81,244 @@ Please note that all applications are subject to approval from the Authorities o
         <div class="col-sm-1 colWidth1">&nbsp;6 </div>
         <div class="col-sm-10 colWidth10" >
         Employment Contract (2 original sets) <br/>
-       &nbsp; • &nbsp;Complete editable PDF <br/>
-       &nbsp; • &nbsp; Stamp each page with company stamp <br/>
-       &nbsp; • &nbsp; Initial every page and sign last page 
+        
+        <ul>
+        <li>Complete editable PDF </li>
+        <li> Stamp each page with company stamp</li>
+        <li>  Initial every page and sign last page </li>
+        </ul>
+        		
+
+       
+       Please note that: <br/> 
+       
+       <ul>
+        <li> The minimum monthly salary of an employee is AED 1,500 (total) or AED 10,000 (if you wish 
+       to sponsor a family in the future). The minimum income of the Person in Charge, Managers, or Shareholders 
+       is AED 10,000. Original Stamped Bank statement must be provided when requesting for salary 
+       certificates in the future. </li>
+        <li> The employment contract for the Person in Charge or Shareholders must be initialled 
+       and signed by ALL Shareholders of the company. </li>
+        </ul>
+
+  
 		</div>
     </div>
+    
+    <div class="form-group row">
+		<div class="col-sm-1 colWidth1">&nbsp;&nbsp; </div>
+        <div class="col-sm-1 colWidth1">&nbsp;7 </div>
+        <div class="col-sm-10 colWidth10" >University Degree Copy (colour) together with legal Arabic translation. (if applicable)
+Please note that University Degrees must be legalized by the concerned authorities in the country of origin or the respective
+Embassy/ Consulate in UAE, as well as the UAE Embassy in the country of origin and UAE Ministry of Foreign Affairs.
+In addition the original University Degree needs to be presented. </div>
+    </div>
+    
+     <div class="form-group row">
+		<div class="col-sm-1 colWidth1">&nbsp;&nbsp; </div>
+        <div class="col-sm-1 colWidth1">&nbsp;7 </div>
+        <div class="col-sm-10 colWidth10" >University Degree Copy (colour) together with legal Arabic translation. (if applicable)
+Please note that University Degrees must be legalized by the concerned authorities in the country of origin or the respective
+Embassy/ Consulate in UAE, as well as the UAE Embassy in the country of origin and UAE Ministry of Foreign Affairs.
+In addition the original University Degree needs to be presented. </div>
+    </div>
+    
+     <div class="form-group row">
+		<div class="col-sm-1 colWidth1">&nbsp;&nbsp; </div>
+        <div class="col-sm-1 colWidth1">&nbsp;8 </div>
+        <div class="col-sm-10 colWidth10" >2 sets of 4 Passport Photos <br/>
+Please note that two (2) DIFFERENT SETS of four (4) Passport Photos will be required. Each set of photos must be taken with dark
+coloured clothing, pure white background, no glasses and no smiling. Photos should be taken in professional photo shop (e.g. Kodak).
+No Passport Photos that have been edited or Passport Photos from photo booths will be accepted. Photo size 4 cm X 5 cm </div>
+    </div>
+    
+    <div class="form-group row">
+		<div class="col-sm-1 colWidth1">&nbsp;&nbsp; </div>
+        <div class="col-sm-1 colWidth1">&nbsp;9 </div>
+        <div class="col-sm-10 colWidth10" >Residence Visa Fee AED 4,650 <br/>
+Work Permit Fee AED 750 <br/>
+Employment Contract Attestation Fee AED 400 <br/>
+Status Adjustment Fee AED 750 (if applicable) </div>
+    </div>
+    
+    
+    
+    
+    
+    
+    
+     <div class="form-group row">
+      <div class="col-sm-1 colWidth1" style="color:#F00408"> B </div>
+      <div class="col-sm-11" >
+      <span style="color:#F00408">Emirates ID and Medical Fitness Test Requirements</span><br/>
+      </div>
+    </div>
+    
+    
+    
+    <div class="form-group row">
+		<div class="col-sm-1 colWidth1">&nbsp;&nbsp; </div>
+        <div class="col-sm-1 colWidth1">&nbsp;I. </div>
+        <div class="col-sm-10 colWidth10" >Documents Required for Emirates ID Registration: </div>
+    </div>
+      <div class="form-group row">
+		<div class="col-sm-1 colWidth1">&nbsp;&nbsp; </div>
+        <div class="col-sm-1 colWidth1">&nbsp;1 </div>
+        <div class="col-sm-10 colWidth10" >Original Passport</div>
+    </div>
+      <div class="form-group row">
+		<div class="col-sm-1 colWidth1">&nbsp;&nbsp; </div>
+        <div class="col-sm-1 colWidth1">&nbsp;2 </div>
+        <div class="col-sm-10 colWidth10" >Original Entry Permit (with Entry Stamp or Status Adjustment) </div>
+    </div>
+      <div class="form-group row">
+		<div class="col-sm-1 colWidth1">&nbsp;&nbsp; </div>
+        <div class="col-sm-1 colWidth1">&nbsp;3 </div>
+        <div class="col-sm-10 colWidth10" >Emirates ID Fee AED 370 <br/>
+Please note that they will give you Emirates ID registration form that will be used for medical fitness test. If you are a first timer to apply for an Emirates ID, capturing of biometrics is required. <br/>
+Please make sure that they will stamp your Emirates ID registration form after capturing your biometrics. </div>
+    </div>
+    
+  	<div class="form-group row"> &nbsp;&nbsp; </div>
+    
+    <div class="form-group row">
+		<div class="col-sm-1 colWidth1">&nbsp;&nbsp; </div>
+        <div class="col-sm-1 colWidth1">&nbsp;II. </div>
+        <div class="col-sm-9" >
+        Documents Required for Medical Fitness Test at Al Baraha Hospital: 
+        </div>
+        <div class="col-sm-1 colWidth1">Quantity: </div>
+    </div>
+    
+    <div class="form-group row">
+		<div class="col-sm-1 colWidth1">&nbsp;&nbsp; </div>
+        <div class="col-sm-1 colWidth1">&nbsp;1 </div>
+        <div class="col-sm-9" > Original Passport </div>
+        <div class="col-sm-1 colWidth1"> 1 </div>
+    </div>
+    <div class="form-group row">
+		<div class="col-sm-1 colWidth1">&nbsp;&nbsp; </div>
+        <div class="col-sm-1 colWidth1">&nbsp;2 </div>
+        <div class="col-sm-9" > Passport Copy (colour) </div>
+        <div class="col-sm-1 colWidth1"> 2 </div>
+    </div>
+     <div class="form-group row">
+		<div class="col-sm-1 colWidth1">&nbsp;&nbsp; </div>
+        <div class="col-sm-1 colWidth1">&nbsp;3 </div>
+        <div class="col-sm-9" > Original Entry Permit (with Entry Stamp or Status Adjustment) <br/>
+         Do not handover the original entry permit. Only show them the original but handover a copy only. </div>
+        <div class="col-sm-1 colWidth1"> 1 </div>
+    </div>
+    <div class="form-group row">
+		<div class="col-sm-1 colWidth1">&nbsp;&nbsp; </div>
+        <div class="col-sm-1 colWidth1">&nbsp;4 </div>
+        <div class="col-sm-9" > Residence Visa Cancellation Copy (if applicable)  </div>
+        <div class="col-sm-1 colWidth1"> 2 </div>
+    </div>
+     <div class="form-group row">
+		<div class="col-sm-1 colWidth1">&nbsp;&nbsp; </div>
+        <div class="col-sm-1 colWidth1">&nbsp;5 </div>
+        <div class="col-sm-9" > Passport Photo (colour) </div>
+        <div class="col-sm-1 colWidth1"> 3 </div>
+    </div>
+    <div class="form-group row">
+		<div class="col-sm-1 colWidth1">&nbsp;&nbsp; </div>
+        <div class="col-sm-1 colWidth1">&nbsp;6 </div>
+        <div class="col-sm-9" > Original Emirates ID Registration Form </div>
+        <div class="col-sm-1 colWidth1">  </div>
+    </div>
+     
+     <div class="form-group row">
+		<div class="col-sm-1 colWidth1">&nbsp;&nbsp; </div>
+        <div class="col-sm-1 colWidth1">&nbsp;7 </div>
+        <div class="col-sm-10 colWidth10" >Tenancy contract copy (if available) </div>
+    </div> 
+    
+    <div class="form-group row">
+		<div class="col-sm-1 colWidth1">&nbsp;&nbsp; </div>
+        <div class="col-sm-1 colWidth1">&nbsp;8 </div>
+        <div class="col-sm-10 colWidth10" >Medical Fitness Test Fee AED 850 <p>
+        Please note that the Medical report once issued should be stamped three (3) times. 
+        (Stamp over Passport Photo, Stamp of Ministry of Health, Stamp “Free from Infectious Diseases”) 
+        </p>
+        <p>
+        Please mention at the beginning of your Medical Fitness Test, that this is for a Fujairah Visa. 
+        </p> <p>
+        Please be informed that you cannot exit the country after entering the United Arab Emirates with 
+        the Entry Permit or after conducting an in-country status adjustment. <br/>
+        </p> <p>
+        Any travel plans should be discussed with us prior to entering the country with the Entry Permit or 
+        prior to conducting an in-country status adjustment. </p>
+        
+        <p> IMPORTANT: </p>
+        
+			<ul> 
+            	<li> Please DO NOT use any private health insurance card to pay for the Medical Fitness Test. 
+                The Medical Fitness Test will only be accepted by the Authorities and the Government if paid in 
+                cash by the applicant. </li>
+            </ul>
+         </div>
+    </div> 
+    
+    
+    
+     <div class="form-group row">
+      <div class="col-sm-1 colWidth1" style="color:#F00408"> C </div>
+      <div class="col-sm-11" >
+      <span style="color:#F00408">Residency Visa Requirements</span><br/>
+      </div>
+    </div>
+    
+     <div class="form-group row">
+		<div class="col-sm-1 colWidth1">&nbsp;&nbsp; </div>
+        <div class="col-sm-1 colWidth1">&nbsp;I. </div>
+        <div class="col-sm-10 colWidth10" >Documents Required for Residence Visa: </div>
+    </div> 
+     <div class="form-group row">
+		<div class="col-sm-1 colWidth1">&nbsp;&nbsp; </div>
+        <div class="col-sm-1 colWidth1">&nbsp;1 </div>
+        <div class="col-sm-10 colWidth10" >Original Passport (Please note that no temporary Passports will be accepted and that the presented Passport should be valid for
+at least another 7 months at the time of visa application. The Passport should have at least three (3) empty pages.) </div>
+    </div> 
+     <div class="form-group row">
+		<div class="col-sm-1 colWidth1">&nbsp;&nbsp; </div>
+        <div class="col-sm-1 colWidth1">&nbsp;2 </div>
+        <div class="col-sm-10 colWidth10" >Original Medical Fitness Test Report (bearing 3 stamps) </div>
+    </div> 
+     <div class="form-group row">
+		<div class="col-sm-1 colWidth1">&nbsp;&nbsp; </div>
+        <div class="col-sm-1 colWidth1">&nbsp;3 </div>
+        <div class="col-sm-10 colWidth10" >Original Entry Permit (with Entry Stamp or Status Adjustment) </div>
+    </div> 
+     <div class="form-group row">
+		<div class="col-sm-1 colWidth1">&nbsp;&nbsp; </div>
+        <div class="col-sm-1 colWidth1">&nbsp;4 </div>
+        <div class="col-sm-10 colWidth10" >Emirates ID Registration Form Copy (colour); 
+        with Emirates ID stamp for first timer applicants.</div>
+    </div> 
+     <div class="form-group row">
+		<div class="col-sm-1 colWidth1">&nbsp;&nbsp; </div>
+        <div class="col-sm-1 colWidth1">&nbsp;5 </div>
+        <div class="col-sm-10 colWidth10" >Company License Copy (colour) valid for at least another 90 days at 
+        time of visa application, or evidence that renewal of the Trade License is under process. </div>
+    </div>
+    
+    <div class="form-group row">
+		<div class="col-sm-1 colWidth1">&nbsp;&nbsp; </div>
+        <div class="col-sm-1 colWidth1">&nbsp;6 </div>
+        <div class="col-sm-10 colWidth10" >Immigration Card Copy Please note that an expired Immigration 
+        Card will result in a visa application being rejected. </div>
+    </div> 
+    
+    
+    <div class="form-group row">
+      <div class="col-sm-1 colWidth1" style="color:#F00408"> D </div>
+      <div class="col-sm-11" >
+      <span style="color:#F00408"> Available Designations </span><br/> 
+      Please note that the designation selected for the visa should be matching the designation selected in the Employment Contract.
+      </div>
+    </div>
+    
     
   </fieldset>
 </div>
